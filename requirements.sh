e1994946125434f27a1063a2b613a9c39b635f67
ProsperousHeart <MyCroftAI@prosperousheart.com>
2017-05-24 16:57:05 -0500

    updated doc for clarity

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,3 @@
-#This file is for packages needed to be installed for the skill to run
+# This file is for packages needed to be installed for the skill to run
+# For this, there is no known extra need
+# https://github.com/facebook/facebook-python-ads-sdk#install-package
